126487d9e92b48cafec48fa922755cc33d2c37ac
Shivam Bhawsar <67222848+cho-zen@users.noreply.github.com>
2023-10-17 01:24:23 +0530

    Create vulnerable_dependencies.py

---
+++ vulnerable_dependencies.py
@@ -0,0 +1,16 @@
+import requests
+
+def fetch_data(url):
+    response = requests.get(url)
+    return response.text
+
+if __name__ == "__main__":
+    url = "https://example.com/sensitive_data"
+    data = fetch_data(url)
+    print(data)
+
+
+'''
+In this code, the requests library is used to make HTTP requests. If the project depends on a version of requests that
+contains a known security vulnerability, CodeQL can identify this as a security issue.
+'''
